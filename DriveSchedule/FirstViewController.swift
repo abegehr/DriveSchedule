2f857f892e78c6ef9369d3cfcb74943c78141e2f
Anton Begehr <a.begehr@gmail.com>
2019-04-07 15:01:11 +0530

    center map on berlin and add point annotation

--- DriveSchedule/FirstViewController.swift
+++ DriveSchedule/FirstViewController.swift
@@ -18,6 +18,22 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // location of Berlin
+        let location = CLLocationCoordinate2DMake(52.523430, 13.411440)
+        
+        // center map on Berlin
+        let span = MKCoordinateSpan.init(latitudeDelta: 0.06, longitudeDelta: 0.06)
+        let region = MKCoordinateRegion(center: location, span: span)
+        Map.setRegion(region, animated: true)
+        
+        // add point annotation
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Annotation"
+        annotation.subtitle = "This is the place!"
+        
+        Map.addAnnotation(annotation)
     }
 
 
