c8a03a33d157300f83d3a7b1ef5f570e89295a07
Anton Begehr <a.begehr@gmail.com>
2019-04-30 22:12:53 +0200

    get today's events of valid calendars and display them on the map by their location

--- DriveSchedule/FirstViewController.swift
+++ DriveSchedule/FirstViewController.swift
@@ -11,14 +11,18 @@ import MapKit
 import CoreLocation
 import HMKit
 import AutoAPI
+import EventKit
 
 
 class FirstViewController: UIViewController {
 
     @IBOutlet weak var Map: MKMapView!
+    // HM
     var vehicleSerialData: [UInt8] = []
     var vehicleAnnotation = MKPointAnnotation()
     var vehilceLocationTimer = Timer()
+    // EK
+    var eventStore = EKEventStore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,22 +33,37 @@ class FirstViewController: UIViewController {
             vehicleSerialData = HMKit.shared.registeredCertificates[0].gainingSerial
         }
         
+        // vehicle location
+        showVehicleLocation()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // calendar events
+        showCalendarEvents()
+    }
+    
+    func showVehicleLocation() {
+        if (self.vehicleSerialData == []) {
+            return print("ERROR Not connected to a vehicle.")
+        }
+        
         // add vehicle location to map
         do {
             try HMTelematics.sendCommand(AAVehicleLocation.getLocation.bytes , serial: vehicleSerialData) { response in
                 if case HMTelematicsRequestResult.success(let data) = response {
                     guard let data = data else {
-                        return print("Missing response data")
+                        return print("ERROR Missing response data")
                     }
                     
                     // parse
                     guard let vehicleLocation = AutoAPI.parseBinary(data) as? AAVehicleLocation else {
-                        return print("Failed to parse Auto API")
+                        return print("ERROR Failed to parse Auto API")
                     }
                     
                     // get coordinates
                     guard let vehicleLocationCoord = vehicleLocation.coordinates?.value else {
-                        return print("Failed to get vehicle location coordinates.")
+                        return print("ERROR Failed to get vehicle location coordinates.")
                     }
                     
                     // successfully got vehicle location
@@ -63,21 +82,16 @@ class FirstViewController: UIViewController {
                     self.Map.addAnnotation(self.vehicleAnnotation)
                 }
                 else {
-                    print("Failed to get vehicle location: \(response).")
+                    print("ERROR Failed to get vehicle location: \(response).")
                 }
             }
         }
         catch {
-            print("Failed to send command:", error)
+            print("ERROR Failed to send command:", error)
         }
         
         
         // check for the current vehicle location once a second
-        scheduleVehicleLocationUpdates()
-        
-    }
-    
-    func scheduleVehicleLocationUpdates() {
         vehilceLocationTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {_ in
             // get current vehicle location
             do {
@@ -114,6 +128,60 @@ class FirstViewController: UIViewController {
         })
     }
 
-
+    func showCalendarEvents() {
+        eventStore.requestAccess(to: EKEntityType.event, completion: {
+            (accessGranted: Bool, error: Error?) in
+            
+            if accessGranted {
+                // access to calendar granted
+                
+                // get today's events from normal calendars
+                // get today's start and end of day
+                let cal = Calendar.current
+                let date = Date()
+                let date0 = cal.startOfDay(for: date)
+                let date24 = cal.date(byAdding: DateComponents(day: 1, second: -1), to: date0)!
+                // get normal calendars
+                var calendars = self.eventStore.calendars(for: EKEntityType.event)
+                calendars = calendars.filter { $0.type == EKCalendarType.calDAV || $0.type == EKCalendarType.local || $0.type == EKCalendarType.subscription || $0.type == EKCalendarType.exchange }
+                print("Getting calendar events between \(date0) and \(date24) of calendars: \(calendars).")
+                var events = self.eventStore.events(matching: self.eventStore.predicateForEvents(withStart: date0, end: date24, calendars: calendars))
+                
+                // remove all-day events
+                events = events.filter { $0.isAllDay == false }
+                
+                print("Loaded events: ", events)
+                
+                // show events on map
+                for event in events {
+                    self.showEventOnMap(event: event)
+                }
+            } else {
+                print("ERROR No access to user's calendar.")
+            }
+        })
+    }
+    
+    func showEventOnMap(event: EKEvent) {
+        guard let location = event.location else {
+            return print("ERROR Could not get event location for event: ", event)
+        }
+        if location == "" {
+            return //print("Empty event location.")
+        }
+        print("Showing event at: ", location)
+        
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(location) { placemarks, error in
+            if let placemark = placemarks?.first, let location = placemark.location {
+                let mark = MKPointAnnotation()
+                mark.coordinate = location.coordinate
+                mark.title = event.title
+                mark.subtitle = event.location
+                self.Map.addAnnotation(mark)
+            }
+        }
+    }
+    
 }
 
